82b4dc438a591515fe777ea1babce11016b06adc
miazarre <mia.g.dev@gmail.com>
2023-06-01 15:01:00 +0200

    Adding React-Helmet + Clean up

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 require('@lottiefiles/lottie-player');
 
 const Error = () => {
   return (
     <>
+      <Helmet>
+        <title>Error 404</title>
+        <meta name="robots" content="noindex" />
+      </Helmet>
       <div className="flex flex-col items-center justify-center">
         <Link to="/" className="mt-40">
           <h2 className="text-center font-main text-7xl sm:text-7xl font-extrabold uppercase text-sky-200 hover:text-yellow-300 tracking-widest">
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,16 @@
+import { Helmet } from 'react-helmet';
+
+import AboutMe from './AboutMe';
 import Stack from './Stack';
 import Projects from './Projects';
 import Resume from './Resume';
-import AboutMe from './AboutMe';
 
 const Home = () => {
   return (
     <>
+      <Helmet>
+        <title>Mia is here !</title>
+      </Helmet>
       <AboutMe />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         <Stack />
