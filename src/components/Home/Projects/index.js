4fb09ee92c08930fd3332799d01564f45eec9cd1
miazarre <mia.g.dev@gmail.com>
2023-06-18 17:08:07 +0200

    Adding new project on Projects section 'Gloup Gloup' + update style

--- src/components/Home/Projects/index.js
+++ src/components/Home/Projects/index.js
@@ -1,4 +1,5 @@
 import intheMood from '../../../assets/images/in-the-mood.png';
+import gloupGloup from '../../../assets/images/gloup-gloup-chrome-ext.png';
 
 const Projects = () => {
   return (
@@ -16,11 +17,28 @@ const Projects = () => {
             <img
               src={intheMood}
               alt="In the Mood for Website"
-              className="w-3/4 sm:w-3/6 border-8 border-cyan-200 rounded-md transition-transform duration-300 ease-in-out transform-gpu hover:scale-125 hover-border"
+              className="w-3/4 sm:w-3/6 border-8 border-amber-100 rounded-md transition-transform duration-300 ease-in-out transform-gpu hover:scale-125 hover-border"
             />
           </a>
           <p className="pt-5 font-second text-stone-950 dark:text-stone-200">
             A website suggesting films and series to suit our moods
+          </p>
+        </div>
+        <div className="mt-10 ml-5">
+          <a
+            href="https://github.com/miazarre/gloup-gloup-chrome-ext"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={gloupGloup}
+              alt="Gloup Gloup Chrome Extension"
+              className="w-2/4 sm:w-2/6 border-8 border-amber-100 rounded-md transition-transform duration-300 ease-in-out transform-gpu hover:scale-125 hover-border"
+            />
+          </a>
+          <p className="pt-10 font-second text-stone-950 dark:text-stone-200">
+            A small Chrome extension that lets you set an hourly reminder to
+            stay hydrated.
           </p>
         </div>
       </div>
